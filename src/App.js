4ce054a0a8e27df8efb7602c432394f7a2a3871f
csadeghi <csadeghi@stanford.edu>
2024-06-07 17:34:46 -0700

    Made the navigation bar
    Now at the top of the page you can navigate between login, mainpage, and post event page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 import Mainpage from './mainpage';
 import Login from './login';
 import Postpage from './postpage';
 
 const App = () => {
   return (
-    <div className="App">
-      <Postpage />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/postpage">Post</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/postpage" element={<Postpage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
 export default App;
-
